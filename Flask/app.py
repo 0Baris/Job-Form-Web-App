1c8d35899169bd5c01a24219ea996ae5f3cc881c
Barış Cem Ant <147310182+0Baris@users.noreply.github.com>
2024-11-30 17:01:04 +0300

    Update app.py

--- Flask/app.py
+++ Flask/app.py
@@ -9,8 +9,8 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 465
 app.config["MAIL_USE_SSL"] = True
-app.config["MAIL_USERNAME"] = ""
-app.config["MAIL_PASSWORD"] = ""
+app.config["MAIL_USERNAME"] = "" ### Mail 
+app.config["MAIL_PASSWORD"] = "" ### App Password -> https://myaccount.google.com/apppasswords
 
 db = SQLAlchemy(app)
 
