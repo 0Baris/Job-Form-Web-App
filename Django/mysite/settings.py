bb692edc84130c873f2ec41a81d0e2e713dccfc5
Barış Cem Ant <147310182+0Baris@users.noreply.github.com>
2024-11-30 17:01:41 +0300

    Update settings.py

--- Django/mysite/settings.py
+++ Django/mysite/settings.py
@@ -131,4 +131,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = '' ### Mail
-EMAIL_HOST_PASSWORD = '' ### App Password
+EMAIL_HOST_PASSWORD = '' ### App Password -> https://myaccount.google.com/apppasswords
